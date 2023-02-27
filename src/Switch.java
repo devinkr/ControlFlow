432cb94a8ce3bd39c0dba7cd0fed12f2141c94df
Devin Raleigh <devinkr@gmail.com>
2023-02-27 15:27:19 -0700

    JDK14 Switch Expression.

--- src/Switch.java
+++ src/Switch.java
@@ -30,25 +30,14 @@ public class Switch {
 
     public static String getQuarter(String month) {
 
-        switch(month) {
-            case "JANUARY":
-            case "FEBRUARY":
-            case "MARCH":
-                return "1st";
-            case "APRIL":
-            case "MAY":
-            case "JUNE":
-                return "2nd";
-            case "JULY":
-            case "AUGUST":
-            case "SEPTEMBER":
-                return "3rd";
-            case "OCTOBER":
-            case "NOVEMBER":
-            case "DECEMBER":
-                return "4th";
-        }
-        return "bad";
+        //JDK 14 switch expression
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> "bad";
+        };
     }
 
 
