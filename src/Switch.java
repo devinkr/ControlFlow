743fecca518abc255f2c42f1fdbb94c26aba36e4
Devin Raleigh <devinkr@gmail.com>
2023-02-27 15:19:26 -0700

    Traditional Switch.

--- src/Switch.java
+++ src/Switch.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Switch {
+public class Switch {
+    public static void main(String[] args) {
+        // if else statements
+//        int value = 3;
+//        if (value == 1) {
+//            System.out.println("Value was 1");
+//        } else if (value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        // With Switch
+        // Can use with byte, short, int, char (Byte, Short, Integer, Character) String, and enum;
+        int switchValue = 3;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, 4, or 5");
+                System.out.println("Actually it was a " + switchValue);
+                break; // if break is not here it will continue until a break or end of switch.
+            default:
+                System.out.println("Was not 1, 2, 3, 4, or 5");
+        }
+        // more code here.
+    }
+
+
 }
