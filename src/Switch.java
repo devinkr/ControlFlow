c3b94e5658150e779d64d7fb9305fd1ca277192a
Devin Raleigh <devinkr@gmail.com>
2023-02-27 15:26:21 -0700

    Traditional Switch with months.

--- src/Switch.java
+++ src/Switch.java
@@ -12,17 +12,43 @@ public class Switch {
 
         // With Switch
         // Can use with byte, short, int, char (Byte, Short, Integer, Character) String, and enum;
-        int switchValue = 3;
-        switch (switchValue) {
-            case 1 -> System.out.println("Value was 1");
-            case 2 -> System.out.println("Value was 2");
-            case 3, 4, 5 -> {
-                System.out.println("Was a 3, 4, or 5");
-                System.out.println("Actually it was a " + switchValue);
-            } // Enhanced switch has no fallthrough so doesn't require breaks.
-            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
+//        int switchValue = 3;
+//        switch (switchValue) {
+//            case 1 -> System.out.println("Value was 1");
+//            case 2 -> System.out.println("Value was 2");
+//            case 3, 4, 5 -> {
+//                System.out.println("Was a 3, 4, or 5");
+//                System.out.println("Actually it was a " + switchValue);
+//            } // Enhanced switch has no fallthrough so doesn't require breaks.
+//            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
+//        }
+//        // more code here.
+
+        String month = "OCTOBER";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+    }
+
+    public static String getQuarter(String month) {
+
+        switch(month) {
+            case "JANUARY":
+            case "FEBRUARY":
+            case "MARCH":
+                return "1st";
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return "2nd";
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return "3rd";
+            case "OCTOBER":
+            case "NOVEMBER":
+            case "DECEMBER":
+                return "4th";
         }
-        // more code here.
+        return "bad";
     }
 
 
